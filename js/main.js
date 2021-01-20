5fd6451a235ef6806b93f0935e7a2829700bbc70
ArthurVolkov <48453179+ArthurVolkov@users.noreply.github.com>
2021-01-20 23:00:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -94,9 +94,10 @@ function renderStart() {
     var elFlags = document.querySelector('.flags');
     var elSmyle = document.querySelector('.smyle');
     elTimer.innerText = '000'
-    elFlags.innerText = gGame.flagsLeft;
+    elFlags.innerText = gGame.flagsLeft + MARK;
     elSmyle.innerHTML = SMYLE;
 }
+
 
 
 function renderBoard(board) {
@@ -106,10 +107,17 @@ function renderBoard(board) {
         for (var j = 0; j < board[0].length; j++) {
             var cell = board[i][j];
             var className = `cell cell${i}-${j}`;
-            var toShow = (cell.isShown) ? cell.minesAroundCount : EMPTY;
+            var toShow = EMPTY;
+            if (cell.isShown) {
+                toShow = cell.minesAroundCount;
+                className += ` shown`
+                // className += getFontColor(cell)
+            }             
+            if (!cell.minesAroundCount) toShow = EMPTY;
             if (cell.isMarked) toShow = MARK;
             if (cell.isMine && !gGame.isOn) toShow = MINE;
             if (cell.isMine && !gGame.isOn && cell.isMarked) toShow = FOUND_MINE;
+            
             strHTML += `<td class="${className}" onclick="cellClicked(this, ${i}, ${j})"
                         oncontextmenu="cellMarked(this, ${i}, ${j}); return false">${toShow}</td>`
         }
@@ -119,6 +127,33 @@ function renderBoard(board) {
     var elContainer = document.querySelector('.board-container');
     elContainer.innerHTML = strHTML;
 }
+
+
+function getFontColor (cell) {
+
+}
+
+
+// function renderBoard(board) {
+//     var strHTML = '<table border="1" cellpadding="1"><tbody>';
+//     for (var i = 0; i < board.length; i++) {
+//         strHTML += '<tr>';
+//         for (var j = 0; j < board[0].length; j++) {
+//             var cell = board[i][j];
+//             var className = `cell cell${i}-${j}`;
+//             var toShow = (cell.isShown) ? cell.minesAroundCount : EMPTY;
+//             if (cell.isMarked) toShow = MARK;
+//             if (cell.isMine && !gGame.isOn) toShow = MINE;
+//             if (cell.isMine && !gGame.isOn && cell.isMarked) toShow = FOUND_MINE;
+//             strHTML += `<td class="${className}" onclick="cellClicked(this, ${i}, ${j})"
+//                         oncontextmenu="cellMarked(this, ${i}, ${j}); return false">${toShow}</td>`
+//         }
+//         strHTML += '</tr>'
+//     }
+//     strHTML += '</tbody></table>';
+//     var elContainer = document.querySelector('.board-container');
+//     elContainer.innerHTML = strHTML;
+// }
 
 
 function cellClicked(elCell, i, j) {
@@ -203,7 +238,7 @@ function cellMarked(elCell, i, j) {
         cell.isMarked = true;
         gGame.markedCount++;
         gGame.flagsLeft--;
-        elFlags.innerText = gGame.flagsLeft;
+        elFlags.innerText = gGame.flagsLeft + MARK;
     }
     renderBoard(gBoard)
     checkGameOver();
